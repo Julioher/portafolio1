aa1cbd2cb9c403fa9441af72c6f8327f64bafaf4
Julioher <jahc-2012@hotmail.com>
2018-05-04 17:47:25 -0600

    primer cambio

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimun-scale=1.0">
+	<link rel="stylesheet" href="general/css/flexboxgrid.min.css">
+	<link rel="stylesheet" href="general/css/estilos.css">
+	<title>PORTAFOLIO</title>
+</head>
+<body>
+	<header>
+      <div class="logo-menu">
+        <div class="container">
+          <div class="row middle-xs between-xs">
+            <div class="logo col-xs-12 col-sm-4 center-xs start-sm"><h1>PORTAFOLIO</h1></div>
+
+          </div>
+        </div>
+      </div>
+
+      <div class="submenu">
+        <div class="container">
+          <div class="row center-xs">
+            <div class="col-xs">
+              <nav>
+                <a href="Cómputo_1/">Cómputo 1</a>
+                <a href="Cómputo_2/">Cómputo 2</a>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+    <div class="main">
+      <div class="container">
+        <div class="row around-xs">
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+           <a href="Cómputo_1/práctica_1JulioHerWebYComercioElectrónico/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+             <figcaption>Práctica 1</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_1/práctica_2JulioHerWebYComercioElectrónico/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+            <figcaption>Práctica 2</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_1/práctica_3JulioHerWebYComercioElectrónico/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+            <figcaption>Práctica 3</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_1/práctica_4JulioHerWebYComercioElectrónico/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+            <figcaption>Práctica 4</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_1/Parcial_Julio_Hernández/">
+            <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt=""></a>
+            <figcaption>Parcial 1</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_2/PrácticaHTML5_Julio_Hernández/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+            <figcaption>Práctica HTML5</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+            <a href="Cómputo_2/Práctica_24-03-2018/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+            </a>
+            <figcaption>Práctica 24/03/2018</figcaption>
+          </div>
+
+          <div class="thumb col-xs-6 col-sm-4 col-md-3 ">
+           <a href="Cómputo_2/Práctica_6_Julio_Hernández/">
+              <img src="https://thumbs.gfycat.com/PaleGargantuanCooter-size_restricted.gif" alt="">
+           </a>
+            <figcaption>Práctica 6</figcaption>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <footer>
+      <div class="container">
+        <div class="row middle-xs between-xs">
+          <div class="col-xs-12 col-sm-4">
+            <div class="nav">
+              <a href="Cómputo_1/práctica_1JulioHerWebYComercioElectrónico/">Práctica 1</a>
+              <a href="Cómputo_1/práctica_2JulioHerWebYComercioElectrónico/">Práctica 2</a>
+              <a href="Cómputo_1/práctica_3JulioHerWebYComercioElectrónico/">Práctica 3</a>
+              <a href="Cómputo_1/práctica_4JulioHerWebYComercioElectrónico/">Práctica 4</a>
+              <a href="Cómputo_1/Parcial_Julio_Hernández/">Parcial 1</a>
+              <a href="Cómputo_2/PrácticaHTML5_Julio_Hernández/">Práctica HTML5</a>
+
+              <a href="Cómputo_2/Práctica_24-03-2018/">Práctica 24/03/2018</a>
+              <a href="Cómputo_2/Práctica_6_Julio_Hernández/">Práctica 6</a>
+            </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4">
+            <div class="logo">PORTAFOLIO</div>
+          </div>
+        </div>
+		</div>
+</body>
+</html>
